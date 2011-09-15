bdb62711b1fd319ba87db8969cb8bb8942bb77d0
Matt Furden <zolrath@gmail.com>
2011-09-15 14:48:47 -0700

    More namespace! Oops.

--- src/andrew_site/views/welcome.clj
+++ src/andrew_site/views/welcome.clj
@@ -1,9 +1,10 @@
 (ns andrew-site.views.welcome
   (:require [andrew-site.views.common :as common]
+            [andrew-site.views.content :as content]
             [noir.content.pages :as pages])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
 
 (defpage "/" []
-  (common/layout (common/list-jobs jobs) (common/sidebar)))
+  (common/layout (common/jobs-list content/jobs) (common/sidebar)))
