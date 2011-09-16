0ed6a2e1b433a77a2de86209737e6e2a8e9580ba
Matt Furden <zolrath@gmail.com>
2011-09-15 18:44:05 -0700

    maybe-init issues

--- src/andrew_site/models/db.clj
+++ src/andrew_site/models/db.clj
@@ -9,7 +9,7 @@
   "Parses mongodb url from heroku, eg. mongodb://user:pass@localhost:1234/db"
   (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)] ;; Setup the regex.
     (when (.find matcher) ;; Check if it matches.
-      (zipmap [:match :user :pass :hOst :port :db] (re-groups matcher))))) ;; Construct an options map.
+      (zipmap [:match :user :pass :host :port :db] (re-groups matcher))))) ;; Construct an options map.
 
 (defn maybe-init []
   "Checks if connection and collection exist, otherwise initialize."
